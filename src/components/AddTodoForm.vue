9419581ea8650dac24c61f5ea2114acfcca5e8c5
dj0nny <frank96990@gmail.com>
2020-08-02 14:43:04 +0200

    handle add new todo

---
+++ src/components/AddTodoForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="add-todo-form">
+    <form @submit.prevent="handleAdd">
+      <fieldset class="uk-fieldset">
+        <legend class="uk-legend">Add Todo</legend>
+        <div class="uk-margin uk-child-width-expand" uk-grid>
+          <div class="uk-width-2-3@m">
+            <input class="uk-input" type="text" placeholder="Todo" v-model="newTodo.text">
+          </div>
+          <div class="uk-width-1-3@m">
+            <button type="submit" class="uk-button-primary">Add Todo</button>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddTodoForm',
+  data: () => ({
+    newTodo: {
+      text: '',
+    },
+  }),
+  methods: {
+    handleAdd() {
+      this.$emit('todoAdded', this.newTodo);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.uk-button-primary {
+  padding: 10px 20px;
+  cursor: pointer;
+  text-transform: uppercase;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="home">
-    home
+    <AddTodoForm @todoAdded="addTodo" />
   </div>
 </template>
 
 <script>
+import AddTodoForm from '../components/AddTodoForm.vue';
+
 export default {
   name: 'Home',
+  data: () => ({
+    todos: [],
+  }),
+  components: {
+    AddTodoForm,
+  },
+  methods: {
+    addTodo(e) {
+      const newTodo = {
+        text: e.text,
+        done: false,
+      };
+      this.todos.push(newTodo);
+      localStorage.setItem('todoList', JSON.stringify(this.todos));
+    },
+  },
+  mounted() {
+    if (localStorage.getItem('todoList') === null) {
+      localStorage.setItem('todoList', JSON.stringify([]));
+    } else {
+      this.todos = JSON.parse(localStorage.getItem('todoList'));
+    }
+  },
 };
 </script>
